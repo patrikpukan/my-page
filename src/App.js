35cfebfb892614e5300955c6ef599028679b35be
Patrik Pukan <patrik.pukan@gmail.com>
2023-03-21 00:24:11 +0100

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './button.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <button>Button 1</button>
+      <button>Button 2</button>
+      <button>Button 3</button>
+      <button>Button 4</button>
+    </nav>
+  );
+}
+
+export default Navbar;
