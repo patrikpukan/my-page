1b11e3a3f512eadf49080e20c46c5c3c95021e5d
Patrik Pukán <patrik.pukan@gmail.com>
2023-04-01 19:34:38 +0200

    added subsections, implemented experience subsection

---
+++ src/components/experience.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './experience.css';
+
+
+function Experience() {
+  return(
+    <React.Fragment>
+      <div className="CVentity">
+        <p className="text1">
+          XiaomiPlanet.sk
+        </p>
+      </div>
+      <p className="text2">
+        Skills
+      </p>
+      <p className="text3">
+        Copywriting - Research - SEO 
+      </p>
+      <div className="CVentityMoved">
+        <p className="text1">
+          eM Client
+        </p>
+        <p className="text2">
+        Skills
+      </p>
+        <p className="text3">
+        Professional Communication - Product Support - Product Testing 
+      </p>
+      </div>
+
+      <div className="CVentity">
+        <p className="text1">
+          HealthMode
+        </p>
+        <p className="text2">
+        Skills
+      </p>
+        <p className="text3">
+        Copywriting - Research - SEO 
+      </p>
+      </div>
+
+      <div className="CVentityMoved">
+        <p className="text1">
+          Progresívne Košice
+        </p>
+        <p className="text2">
+        Skills
+        </p>
+        <p className="text3">
+        Facebook & Instagram Account Management - Adobe Premiere & Photoshop - Content Management - Professional Communication 
+      </p>
+      </div>
+    </React.Fragment>
+  );
+}
+export default Experience;
